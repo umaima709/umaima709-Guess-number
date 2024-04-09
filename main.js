3aaa3d2ca05bda509d75d3116dd8a5b80ee4a4b8
umaima <113837633+umaima709@users.noreply.github.com>
2024-04-09 18:33:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const input = await inquirer.prompt([{
+        name: " userNum",
+        type: "number",
+        message: "Guess a number from 1 to 6",
+    }]);
+if (input.userNum === randomNumber) {
+    console.log("congratulation you guess correct number its " + randomNumber);
+}
+else {
+    console.log("Its wrong number try again!");
+}
